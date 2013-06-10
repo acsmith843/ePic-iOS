430b17e44e06b85c41d92fa1e24d25e4f5d606d4
Adam C. Smith <adam.smith@sparcedge.com>
2013-06-10 15:50:21 -0400

    Initial Commit

---
+++ ePic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ePic
+//
+//  Created by Adam Smith on 6/10/13.
+//  Copyright (c) 2013 acs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
