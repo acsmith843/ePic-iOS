881481cea11fbddf025967ce539a9c57fdc5e1d4
Adam C. Smith <adam.smith@sparcedge.com>
2013-06-12 11:48:52 -0400

    project, storyboard, and pch

--- ePic/AppDelegate.h
+++ ePic/AppDelegate.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "User.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong) User *currentUser;
 
 @end
