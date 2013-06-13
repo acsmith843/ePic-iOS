da3c189b95cfb3d6873d3973c20158c2d1aed451
Adam C. Smith <adam.smith@sparcedge.com>
2013-06-13 16:21:21 -0400

    renaming login view controller

--- ePic/LoginViewController.m
+++ ePic/LoginViewController.m
@@ -6,35 +6,62 @@
 //  Copyright (c) 2013 acs. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
 #import "AFHTTPClient.h"
 #import "AFJSONRequestOperation.h"
 #import "User.h"
 #import "Album.h"
 #import "AppDelegate.h"
 #import "AlbumsViewController.h"
+#import "UserManager.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
 @property (nonatomic, strong) User *currentUser;
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // register for the session change notification
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+        selector:@selector(sessionStateChanged:)
+        name:FBSessionStateChangedNotification
+        object:nil];
+    
+    // Check the session for a cached token to show the proper authenticated
+    // UI. However, since this is not user intitiated, do not show the login UX.
+    BOOL signedIn = [APP_DELEGATE openSessionWithAllowLoginUI:NO];
+    
+    if(signedIn){
+        
+        int64_t delayInSeconds = 0.5;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [self dismissViewControllerAnimated:NO completion:nil];
+        });
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    // unregister for notifications
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void) viewWillAppear:(BOOL)animated {
     
-    [self findUser];
+    [super viewWillAppear:animated];
+    
+//    [self findUser];
     
 }
 
@@ -94,6 +121,23 @@
     }];
     
     [operation start];
+}
+
+- (IBAction)performLogin:(id)sender {
+    
+//    [self.spinner startAnimating];
+    
+    [APP_DELEGATE openSessionWithAllowLoginUI:YES];
+    
+    UserManager *userManager = [[UserManager alloc] init];
+    [userManager findUserByFacebookId];
+}
+
+- (void)sessionStateChanged:(NSNotification*)notification {
+    //if success then dismiss the modal
+    if (FBSession.activeSession.isOpen) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
 }
 
 @end
