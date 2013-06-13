ff1dd4e315e4f1a7d19d9fd27cb9a9e2e67f5911
Adam C. Smith <adam.smith@sparcedge.com>
2013-06-13 16:21:58 -0400

    adding constants file

---
+++ ePic/Constants.h
@@ -0,0 +1,11 @@
+//
+//  Constants.h
+//  ePic
+//
+//  Created by Adam Smith on 6/12/13.
+//  Copyright (c) 2013 acs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const URL_LOCAL_BASE;
---
+++ ePic/Constants.m
@@ -0,0 +1,11 @@
+//
+//  Constants.m
+//  ePic
+//
+//  Created by Adam Smith on 6/12/13.
+//  Copyright (c) 2013 acs. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString *const URL_LOCAL_BASE = @"http://localhost:9000/v1/";
