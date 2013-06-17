34c30f5c06734f25ee867ef1e59cf37dd918aca2
Adam C. Smith <adam.smith@sparcedge.com>
2013-06-17 14:50:23 -0400

    adding create album controller

---
+++ ePic/CreateAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateAlbumViewController.h
+//  ePic
+//
+//  Created by Adam Smith on 6/17/13.
+//  Copyright (c) 2013 acs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateAlbumViewController : UIViewController
+
+@end
